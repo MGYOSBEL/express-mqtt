61085eacd0a6c4023f077ceb308cb21f5790b0d4
MGYOSBEL <mgyosbel@gmail.com>
2021-01-07 11:29:38 +0100

    all working OK, but all mixed in the app.js file

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mqtt = require('mqtt');
+var firebase = require('firebase/app');
+
+require('firebase/database');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBOQYVPKxEhd4qF0NXEnrqXQzMTJvfmGls",
+  authDomain: "temperature-catalunia.firebaseapp.com",
+  databaseURL: "https://temperature-catalunia-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "temperature-catalunia",
+  storageBucket: "temperature-catalunia.appspot.com",
+  messagingSenderId: "749669114352",
+  appId: "1:749669114352:web:11866e8b0d4c95ed54bd35",
+  measurementId: "G-KCV2CFQKYC"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var mqttClient = mqtt.connect('http://127.0.0.1:11883');
 
 var app = express();
 
@@ -37,5 +56,24 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+mqttClient.on('connect', function () {
+  mqttClient.subscribe('temperatura/#', function (err) {
+    if (!err) {
+      // mqttClient.publish('presence', 'Hello mqtt')
+      console.log('node mqttClient subscribed....');
+    }
+  })
+})
+ 
+mqttClient.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(topic, message.toString());
+  const date = new Date();
+  firebase.database().ref(`${topic}/${date}`).set({
+    date: date,
+    temperature: message.toString()
+  });
+})
 
 module.exports = app;
